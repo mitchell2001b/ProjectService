4ccd44bfd87dc2851069112c6c65a33c942e0814
mitchell2001b <79633852+mitchell2001b@users.noreply.github.com>
2023-11-28 13:12:21 +0100

    fixed the producer

--- src/main/java/com/example/ProjectService/Configuration/KafkaConfiguration.java
+++ src/main/java/com/example/ProjectService/Configuration/KafkaConfiguration.java
@@ -3,8 +3,10 @@ package com.example.ProjectService.Configuration;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
+@Configuration
 public class KafkaConfiguration
 {
     @Value("${spring.kafka.producer.topic.name}")
--- src/main/java/com/example/ProjectService/kafka/ProjectProducer.java
+++ src/main/java/com/example/ProjectService/kafka/ProjectProducer.java
@@ -11,7 +11,9 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ProjectProducer
 {
     private static final Logger LOGGER = LoggerFactory.getLogger(ProjectProducer.class);
