77de162efb19cd0984dd15ec8f7f600ac3c2a1d5
mitchell2001b <79633852+mitchell2001b@users.noreply.github.com>
2023-12-13 21:51:45 +0100

    testing new origin allowance

---
+++ src/main/java/com/example/ProjectService/kafka/RegistrationDeleteConsumer.java
@@ -0,0 +1,51 @@
+package com.example.ProjectService.kafka;
+
+import com.example.ProjectService.ProjectMember.ProjectMember;
+import com.example.ProjectService.repositories.IAccountRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+
+@Service
+public class RegistrationDeleteConsumer
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger(RegistrationConsumer.class);
+    private DateTimeFormatter DateTimeFormatterEvent = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    private ObjectMapper objectMapper;
+
+    private IAccountRepository accountRepo;
+
+    @Autowired
+    public RegistrationDeleteConsumer(ObjectMapper givenObjectMapper, IAccountRepository accountRepository)
+    {
+        this.objectMapper = givenObjectMapper;
+        this.accountRepo = accountRepository;
+    }
+
+    @KafkaListener(topics = "${spring.kafka.consumer2.topic.name}", groupId = "${spring.kafka.consumer2.group-id}")
+    public void consume(String jsonEvent) {
+        try
+        {
+            Map<String, Object> eventMap = objectMapper.readValue(jsonEvent, new TypeReference<Map<String, Object>>() {
+            });
+
+            Number accountIdNumber = (Number) eventMap.get("id");
+            Long accountId = accountIdNumber.longValue();
+
+            LOGGER.info(String.format("User created event received in project service => %s", accountId.toString()));
+        }
+        catch(JsonProcessingException e)
+        {
+            LOGGER.error("Error deserializing JSON message", e);
+        }
+    }
+
+}
