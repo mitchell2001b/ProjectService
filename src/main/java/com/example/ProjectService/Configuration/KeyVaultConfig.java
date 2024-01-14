ee23cc854a617225016b3e757c553b55b46d66ef
mitchell2001b <79633852+mitchell2001b@users.noreply.github.com>
2024-01-14 15:10:26 +0100

    added authentication support + micrometer for custom metrics

--- src/main/java/com/example/ProjectService/Configuration/KeyVaultConfig.java
+++ src/main/java/com/example/ProjectService/Configuration/KeyVaultConfig.java
@@ -6,10 +6,11 @@ import com.azure.security.keyvault.secrets.SecretClient;
 import com.azure.security.keyvault.secrets.SecretClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
-
+@Configuration
 public class KeyVaultConfig {
    /* @Bean
     public SecretClient secretClient() {
